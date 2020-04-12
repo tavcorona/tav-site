6c901da77f304d0df42c68714e30c6eba171edaa
tavcorona <63522446+tavcorona@users.noreply.github.com>
2020-04-12 19:02:07 +0100

    t

--- components/web/src/App.js
+++ components/web/src/App.js
@@ -7,9 +7,9 @@ const App = () =>
       <h1>
         The default landing page for the home of the "Help My Neighbor" project.
       </h1>
-      <h2>A tool designed to match willing helpers with the neighbors in need of help.</h2>
+      <h2>Made with <span role="img" aria-label="love">💖</span> by working remotely<span role="img" aria-label="world">🌍</span>.</h2>
       <p>
-        Made with <span role="img" aria-label="love">💖</span> by working remotely<span role="img" aria-label="world">🌍</span>
+        A tool designed to match willing helpers with the neighbors in need of help.
       </p>
     </header>
   </div>;
