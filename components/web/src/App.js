bc998ae4ce28fd3b90709f2ad75eecbe63ce1162
tavcorona <63522446+tavcorona@users.noreply.github.com>
2020-04-12 18:55:33 +0100

    Redesign.

--- components/web/src/App.js
+++ components/web/src/App.js
@@ -5,12 +5,11 @@ const App = () =>
   <div className="App">
     <header className="App-header">
       <h1>
-        The default landing page for the home of the "Help My Neighbor" project. Made with <span role="img" aria-label="love">💖</span>
-by working remotely<span role="img" aria-label="world">🌍</span>.
+        The default landing page for the home of the "Help My Neighbor" project.
       </h1>
-      <h2><span role="img" aria-label="Help My Neighbor">🏃</span></h2>
+      <h2>A tool designed to match willing helpers with the neighbors in need of help.</h2>
       <p>
-        A tool designed to match willing helpers with the neighbors in need of help.
+        Made with <span role="img" aria-label="love">💖</span> by working remotely<span role="img" aria-label="world">🌍</span>
       </p>
     </header>
   </div>;
